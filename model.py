b7dcd1ef79540decd5ab2632d31cf33a16bdaf04
funkyymonkey <49716268+funkyymonkey@users.noreply.github.com>
2020-05-25 18:46:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -19,7 +19,9 @@ classifier.add(Dense(units=6, activation='softmax')) # softmax for more than 2
 classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) 
 x=1
 history=classifier.fit_generator(training_set,steps_per_epoch=600,epochs=x,validation_data=test_set,validation_steps=30)
+acc=history.history['val_accuracy'][0]
+acc=(int(round(acc,2) * 100))                                       
 file=open("acc.txt","w")
-pp=str(history.history['val_accuracy'][0])
+pp=str(acc)
 file.write(pp)
 file.close()
